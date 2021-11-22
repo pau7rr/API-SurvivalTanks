37d87f57f735231210108c829c1cba8999a88b13
pau7rr <pau.rosquellas@gmail.com>
2021-11-22 15:29:08 +0100

    fix devlog import

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,4 @@
 <?php
-
-use App\Http\Controllers\DevlogController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\Auth\AdminAuthController;
