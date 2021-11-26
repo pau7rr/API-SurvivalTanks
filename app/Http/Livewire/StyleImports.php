7b11e20a4dff8965fc0bbd7c98116d72f6866a42
David Masip <davidmasip16@gmail.com>
2021-11-26 18:35:17 +0100

    Livewire Components
    Navbar / Styles / Scripts

---
+++ app/Http/Livewire/StyleImports.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class StyleImports extends Component
+{
+    public function render()
+    {
+        return view('livewire.style-imports');
+    }
+}
