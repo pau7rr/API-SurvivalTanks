f3904c49c1061c008f27eb3b5797402dddc10929
pau7rr <pau.rosquellas@gmail.com>
2022-01-14 16:50:53 +0100

    livewire resources fixed

--- resources/views/devlogs/devlogs.blade.php
+++ resources/views/devlogs/devlogs.blade.php
@@ -9,6 +9,9 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+    <!-- Styles -->
+    @livewireStyles
+    @livewire('style-imports')
     
 </head>
 
@@ -58,6 +61,9 @@
 </body>
 
 </html>
+
+@livewireScripts
+
 <script src="{{ asset('js/app.js') }}"></script>
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
 <script>
