304b686658fa3f626ffcbe16a5b09889111a6976
Pau Rosquellas <90578037+pau7rr@users.noreply.github.com>
2022-01-14 16:42:42 +0100

    livwire to devlogs (#9)
    * Several modifications
    
    Tank Editor page / Admins Migration / Admins Seeder
    
    * Livewire Components (#8)
    
    Navbar / Styles / Scripts
    
    Co-authored-by: David Masip <davidmasip16@gmail.com>

--- resources/views/administrative/dashboard.blade.php
+++ resources/views/administrative/dashboard.blade.php
@@ -5,68 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="icon" href="../img/logo/logo_icon_100x100_black.png">
-
     
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/general.css') }}" rel="stylesheet">
+    @livewireStyles
+    @livewire('style-imports')
     <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
+    
+    
 </head>
 <body>
-    <!-- Navbar -->
-    <div class="container.fluid sticky-top mb-5">
-        <nav class="navbar navbar-expand-md bg-dark-blue navbar-dark border-bottom mt-3 mb-3" role="navigation">
-                <!--Navbar Logo -->
-                <a class="navbar-brand" href="/">
-                    <img src="../img/logo/logo_500x200_white.png" width="100" height="50"/>
-                </a>
-                <!--Navbar Button Toggler-->
-                <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarContent">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <!--Navbar Links-->
-                <div class="collapse navbar-collapse" id="navbarContent">
-                    <div class="container-fluid">
-                        <ul class="nav navbar-nav">
-                            <li class="nav-item">   
-                                <a class="nav-link" href="dashboard">Dashboard</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="tankEditor">Tank Editor</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="devlogs">Devlog</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="botiga">Botiga</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="noticies">Notícies</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="resultats">Resultats</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="/contacte">Contacte</a>
-                                <div class="dropdown-menu">
-                                    <a href="/contactar" class="dropdown-item">Contactar Staff</a>
-                                    <a href="/quisom" class="dropdown-item">Qui som</a>
-                                </div>
-                            </li>
-                        </ul>
-                        <ul class="nav navbar-nav">
-                            <li class="nav-item">
-                                <a href="logout" class="nav-link px-3 bg-dark-green rounded rounded-pill">
-                                    <!--<img src="" width="15" height="15"/>--> Logout
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    
-                </div>
-        </nav>
-    </div>
+    @livewire('navbar')
     <!-- Title -->
     <div class="container">
         <div class="text-center">
@@ -77,6 +25,7 @@
 
 
 
-
 </body>
 </html>
+@livewireScripts
+@livewire('script-imports')
---
+++ resources/views/livewire/style-imports.blade.php
@@ -0,0 +1,3 @@
+<link rel="icon" href="../img/logo/logo_icon_100x100_black.png">
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('css/general.css') }}" rel="stylesheet">
