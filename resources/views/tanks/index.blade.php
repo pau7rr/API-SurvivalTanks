062b5e2615f697d4b3451130263ff8d84b303153
David Masip <davidmasip16@gmail.com>
2022-01-15 00:48:10 +0100

    Update finished

--- resources/views/tanks/index.blade.php
+++ resources/views/tanks/index.blade.php
@@ -9,15 +9,10 @@
     <!-- Styles -->
     @livewireStyles
     @livewire('style-imports')
-    
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.0/css/buttons.dataTables.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
 </head>
 <body>
-
     @livewire('navbar')
     
-
     <!-- Tanks List -->
     <div class="container mb-5">
         <!-- Errors Return -->
@@ -78,16 +73,5 @@
 </body>
 </html>
 @livewireScripts
-<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
 
-<script>
-    $(document).ready(function () {
-        var table = $('#datatable').DataTable();
-    });
-</script>
-
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
