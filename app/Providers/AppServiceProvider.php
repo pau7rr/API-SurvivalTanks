0f17cf9dcfa5335ef6ef2b1e788691fe205882c0
pau7rr <pau.rosquellas@gmail.com>
2022-01-16 16:40:11 +0100

    URL imported

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\URL;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
