6c442f9e2982c44a93717191d013735ded03a328
David Masip <davidmasip16@gmail.com>
2022-02-25 08:10:19 +0100

    Create UserTankSeeder.php

---
+++ database/seeders/UserTankSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserTankSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        UserTank::create(
+            [
+                'strengh' => 5,
+                'health' => 5,
+                'speed' => 5,
+                'tower' => '../assets/tankConstructor/TowerEdits/Light1.png',
+                'body' => '../assets/tankConstructor/Hulls/SmallHullA.png',
+                'track' => '../assets/tankConstructor/Tracks/TrackAFrame1.png',
+                'bullet' => '../assets/tankConstructor/Bullets/01.png',
+            ]            
+        );
+        UserTank::create(
+            [
+                'strengh' => 5,
+                'health' => 5,
+                'speed' => 5,
+                'tower' => '../assets/tankConstructor/TowerEdits/Light1.png',
+                'body' => '../assets/tankConstructor/Hulls/SmallHullA.png',
+                'track' => '../assets/tankConstructor/Tracks/TrackAFrame1.png',
+                'bullet' => '../assets/tankConstructor/Bullets/01.png',
+            ]            
+        );
+        UserTank::create(
+            [
+                'strengh' => 5,
+                'health' => 5,
+                'speed' => 5,
+                'tower' => 'http://localhost:8000/assets/tankConstructor/TowerEdits/Light2.png',
+                'body' => 'http://localhost:8000/assets/tankConstructor/TowerEdits/Light2.png',
+                'track' => 'http://localhost:8000/assets/tankConstructor/TowerEdits/Light2.png',
+                'bullet' => 'http://localhost:8000/assets/tankConstructor/TowerEdits/Light2.png',
+            ]            
+        );
+        UserTank::create(
+            [
+                'strengh' => 5,
+                'health' => 5,
+                'speed' => 5,
+                'tower' => 'http://localhost:8000/assets/tankConstructor/TowerEdits/Light2.png',
+                'body' => 'http://localhost:8000/assets/tankConstructor/TowerEdits/Light2.png',
+                'track' => 'http://localhost:8000/assets/tankConstructor/TowerEdits/Light2.png',
+                'bullet' => 'http://localhost:8000/assets/tankConstructor/TowerEdits/Light2.png',
+            ]            
+        );
+    }
+}
