da19dc3ec08572a2ddba5151c69a1ca604eb7fd1
David Masip <davidmasip16@gmail.com>
2022-04-11 20:00:54 +0200

    View and URLs Updated

--- app/Http/Controllers/Administrative/DashboardController.php
+++ app/Http/Controllers/Administrative/DashboardController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers\Administrative;
 
+use App\Models\Stats;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     function dashboard(){
-        return view('administrative.dashboard');
-    }
+        $stats = Stats::get();
+        //Compact data sent to view
+        $data = [
+            'stats' => $stats
+        ];
+        return view('administrative.dashboard', $data);}
 }
